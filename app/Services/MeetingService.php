c1f6872293b7723299569bfa7560de0860ede70c
lratana <samchansreyma@gmail.com>
2026-06-15 17:05:39 +0700

    update Meeting Service

--- app/Services/MeetingService.php
+++ app/Services/MeetingService.php
@@ -9,18 +9,39 @@ class MeetingService
 {
     public function syncMeetingStatuses(): void
     {
-        $now = Carbon::now();
+        // $now = Carbon::now();
 
-        Booking::where('status', 'approved')
-            ->where('end_datetime', '<', $now)
+        // Booking::where('status', 'approved')
+        //     ->where('end_datetime', '<', $now)
+        //     ->update([
+        //         'status' => 'completed'
+        //     ]);
+
+        // Booking::where('status', 'ongoing')
+        //     ->where('end_datetime', '<', $now)
+        //     ->update([
+        //         'status' => 'completed'
+        //     ]);
+
+        //     $now = now();
+
+        // Approved meeting starts automatically
+        Booking::query()
+            ->where('status', 'approved')
+            ->where('start_datetime', '<=', $now)
+            ->where('end_datetime', '>', $now)
             ->update([
-                'status' => 'completed'
+                'status' => 'in_meeting',
+                'updated_at' => $now,
             ]);
 
-        Booking::where('status', 'ongoing')
-            ->where('end_datetime', '<', $now)
+        // Meeting ends automatically
+        Booking::query()
+            ->whereIn('status', ['approved', 'in_meeting'])
+            ->where('end_datetime', '<=', $now)
             ->update([
-                'status' => 'completed'
+                'status' => 'completed',
+                'updated_at' => $now,
             ]);
     }
 }
